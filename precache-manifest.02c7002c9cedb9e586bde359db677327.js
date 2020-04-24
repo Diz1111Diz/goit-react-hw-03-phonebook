4ca6e21106a8ef671e0e5133d1ff582eccd7a671
unknown <kavunv@gmail.com>
2020-04-24 21:36:17 +0300

    Updates

---
+++ precache-manifest.02c7002c9cedb9e586bde359db677327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff41c7ec742e9350aa73b98acf74e68",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c717e1ae64eac483becd",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.2039741e.chunk.css"
+  },
+  {
+    "revision": "ae65bda22f0c16aeee2c",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0aefecf6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0aefecf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c717e1ae64eac483becd",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.4682b0be.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
